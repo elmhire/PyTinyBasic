1376ab26af08ad526791e0f5573baec1e90ce186
Peter Wirtz <pwirtz13@gmail.com>
2015-11-12 23:34:17 -0800

    Forgot to add main.py to last push

--- main.py
+++ main.py
@@ -4,19 +4,13 @@ from pyTBasic import parser
 
 
 if __name__ == '__main__':
-    test = 'PRINT "+ 2 + A * 4 is ", + B + A * 4, " and 3 + 3 is ", 3 + 3'
-    #test = 'PRINT X'
-    test2 = 'LET X = A + -B'
-    test3 = 'GOTO 3'
-    test4 = 'INPUT A, B, C'
-
-    tokens = parser.generate_tokens(test)
-    for item in tokens:
-        print(item)
-    print()
-
-    e = parser.BasicParser()
-    x = e.parse(test)
-    print(x)
-    for i in x:
-        print(type(i), i)
+    b_parser = parser.BasicParser()
+    _input = ''
+    while _input != 'exit()':
+        _input = input("> ")
+        try:
+            out = b_parser.parse(_input)
+        except SyntaxError as e:
+            print("SYNTAX ERROR ", e)
+        else:
+            print(out)
